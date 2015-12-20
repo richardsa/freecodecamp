855673553e3bc2aad6984cc91c0c29577e40476e
richie <rsaldiva@gmail.com>
2015-12-19 19:29:17 -0500

    finish calculator

--- basic-front-end/calculator/js/custom.js
+++ basic-front-end/calculator/js/custom.js
@@ -1,33 +1,48 @@
 $(function() {
+  
+  //declare global variables
   var x = "";
   var formula = "";
   var solution = "";
-
+  
+  // function that builds formula for calculation
   function formulaBuilder(input) {
     formula = formula + x + input;
     x = "";
     $("#calc-screen").text(x);
   };
-
+  
+  // function for clearing screen
+  function clearScreen() {
+    $("#calc-screen").text("0");
+  };
+  
+  // listener function checking for number button clicks
   $(".calc-number").on("click", function() {
     x = x + $(this).text();
     $("#calc-screen").text(x);
 
   });
+  
+  // listener function checking for symbol button clicks
   $(".control").on("click", function() {
-    if ($(this).text() === "AC") {
-      $("#calc-screen").text("");
+    if ($(this).is('#all-clear')) {
+      clearScreen();
       x = "";
       formula = "";
     } else if ($(this).is("#clear-entry")) {
-      $("#calc-screen").text("");
+      clearScreen();
       x = "";
 
     } else if ($(this).is("#equal")) {
       formula = formula + x;
+      var lastChar = formula.substr(formula.length - 1)
+      if (isNaN(lastChar) && lastChar !== ")") {
+        formula = formula.substring(0, formula.length - 1);
+      }
       $("#calc-screen").text(eval(formula));
       x = "";
-      formula = "";
+      //formula = "";
 
     } else if ($(this).is("#add")) {
       formulaBuilder("+");
@@ -41,14 +56,13 @@ $(function() {
     } else if ($(this).is("#divide")) {
       formulaBuilder("/");
 
-    } else if ($(this).is("#decimal")) {
-      formulaBuilder(".");
+    } else if ($(this).is("#open-paren")) {
+      formulaBuilder("(");
 
-    } else {
-      formula = formula + x + "*"; // $(this).text();
-      x = "";
-      $("#calc-screen").text(x);
-    }
+    } else if ($(this).is("#close-paren")) {
+      formulaBuilder(")");
+
+    } 
   });
 
 });
